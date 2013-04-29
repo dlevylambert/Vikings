03d576c1b4e549b5f6fde57c5f8e225cc13b71e1
dkurk <brainmonkey95@gmail.com>
2013-04-28 20:49:18 -0400

    profile display working

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     loadProfile();
-});
+})
 
 function loadProfile() {
+    $("#userInfo").empty();
     $.getJSON("/getUserInfo", function(data) {
-//need to load all of that data in soon	
+	//need to load all of that data in soon	
+	console.log(data);
+	$("#userInfo").append('<p>'+data[0]+'</p>');
+	$("#userInfo").append('<p>'+data[1]+'</p>');
+	$("#userInfo").append('<p>'+data[2]+'</p>');
+	$("#userInfo").append('<p>'+data[3]+'</p>');
     });
 }
